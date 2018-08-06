28594980150c0f333769d912918edc276d4f2db4
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-06 09:23:02 -0400

    Enabled JSON stdout logging and HTTP connection pool (#117)

--- biblib/client.py
+++ biblib/client.py
@@ -1,4 +1,3 @@
-import requests
 from flask import current_app
 
 client = lambda: Client(current_app.config).session
@@ -6,7 +5,7 @@ client = lambda: Client(current_app.config).session
 
 class Client:
     """
-    The Client class is a thin wrapper around requests; Use it as a centralized
+    The Client class is a thin wrapper around adsmutils ADSFlask client; Use it as a centralized
     place to set application specific parameters, such as the oauth2
     authorization header
     """
@@ -17,7 +16,7 @@ class Client:
         :param config: configuration dictionary of the client
         """
 
-        self.session = requests.Session()
+        self.session = current_app.client # Use HTTP pool provided by adsmutils ADSFlask
         self.token = config.get('BIBLIB_CLIENT_ADSWS_API_TOKEN')
         if self.token:
             self.session.headers.update(
