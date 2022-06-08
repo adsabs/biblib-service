c11962e1b6e2f2a36658215e9a93232418905998
tjacovich <tjacovich@cfa.harvard.edu>
2022-06-08 12:59:37 -0400

    Added mock methods to make unit_tests to properly behave with new bibcode validation.

--- biblib/tests/base.py
+++ biblib/tests/base.py
@@ -471,6 +471,41 @@ class MockSolrQueryService(MockADSWSAPI):
         HTTPretty.reset()
         HTTPretty.disable()
 
+def SolrQueryServiceresp(**kwargs):
+    if kwargs.get('canonical_bibcode'):
+        docs = []
+        canonical_bibcodes = kwargs.get('canonical_bibcode')
+        for i in range(len(canonical_bibcodes)):
+            docs.append({'bibcode': canonical_bibcodes[i]})
+            print(docs)
+        input_query ="identifier%3A("+"%20OR%20".join(canonical_bibcodes)+")"
+    else:
+        docs = [{'bibcode': 'bibcode'} for i
+                in range(kwargs.get('number_of_bibcodes', 1))]
+        input_query = ""
+
+    resp = {
+        'responseHeader': {
+            'status': 0,
+            'QTime': 152,
+            'params': {
+                'fl': 'bibcode',
+                'q': input_query,
+                'wt': 'json'
+            }
+        },
+        'response': {
+            'numFound': len(docs),
+            'start': 0,
+            'docs': docs
+        }
+    }
+
+    #resp = resp.get('response')
+
+    return resp
+
+
 class MockEmailService(MockADSWSAPI):
 
     """
