2bee341883f2273f8263871f1b61a0d9ed8854e3
tjacovich <tjacovich@cfa.harvard.edu>
2022-06-14 09:37:39 -0400

    Copied new environment variables for search url and bigquery min number of entries from local_config to config.

--- config.py
+++ config.py
@@ -28,9 +28,11 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'unset-env').lower()
 BIBLIB_TWOPOINTOH_SERVICE_URL = 'https://api.adsabs.edu/v1/harbour'
 BIBLIB_CLASSIC_SERVICE_URL = 'https://api.adsabs.edu/v1/harbour'
 BIBLIB_SOLR_BIG_QUERY_URL = 'https://api.adsabs.search/v1/bigquery'
+BIBLIB_SOLR_SEARCH_URL = 'https://api.adsabs.harvard.edu/v1/search/query'
 BIBLIB_USER_EMAIL_ADSWS_API_URL = 'https://api.adsabs.harvard.edu/v1/user'
 BIBLIB_ADSWS_API_DB_URI = 'sqlite:////tmp/test.db'
 BIBLIB_MAX_ROWS = 2000
+BIBLIB_SOLR_BIG_QUERY_MIN = 10
 
 MAIL_DEFAULT_SENDER = 'no-reply@adslabs.org'
 
