04fd7ccfbaf31f0fe5eaf57e0edebe3702e578bf
tjacovich <tjacovich@cfa.harvard.edu>
2022-06-23 12:00:05 -0400

    Modified pytest scripts to allow persistent and transient containers to be handled by the same docker-compose.

--- scripts/pytest/pytest.sh
+++ scripts/pytest/pytest.sh
@@ -3,7 +3,9 @@ cd /app
 
 py.test
 
-echo "For interactive access, run in a diferent terminal:"
-echo "  docker exec -it pytest_biblib_microservice bash"
-echo "Press CTRL+c to stop"
-tail -f /dev/null
+if [[ "$1" = "-p" ]]; then
+    echo "For interactive access, run in a diferent terminal:"
+    echo "  docker exec -it pytest_biblib_microservice bash"
+    echo "Press CTRL+c to stop"
+    tail -f /dev/null
+fi
