6a4bdab6b7ea93c95e8fe4e07087373c0f42571f
tjacovich <tjacovich@cfa.harvard.edu>
2023-10-10 12:10:37 -0400

    Additional tweak to env.py

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,6 @@ from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 import os
 import sys
-#import biblib.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
